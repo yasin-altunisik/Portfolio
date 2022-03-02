cab4d8b0de9349c65efd87a98e60df71902e1ef4
Yasin Altunisik <altun.yasin.isik@gmail.com>
2022-03-02 15:17:31 +0300

    test

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <div className="flex">
           <Profile />
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />}  />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
             <Route path="projects" element={<Projects />} />
